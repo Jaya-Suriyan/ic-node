01d05ec1847c2080b5754e938254dc743c2c67ff
Jaya Suriyan <jayasuriyan14india@gmail.com>
2024-08-28 00:06:20 +0530

    implement remote pg

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const errorHandler = require("./helper/errorHandler");
 const morgan = require("morgan");
 const logger = require("./helper/logger");
-const sequelize = require("./helper/db");
 
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
@@ -34,13 +33,5 @@ app.get("/", (_req, res) => res.json({ msg: "Hello, Nodejs!" }));
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
 
-sequelize
-  .sync({ force: true })
-  .then(() => {
-    console.log("DB Synced successfully!");
-  })
-  .catch((error) => {
-    console.error("Unable to sync DB: ", error);
-  });
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
